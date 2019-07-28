da7fb4cd61a2d71123c78d74059555029dea4e54
Davi Ferreira <contato@daviferreira.com>
2019-07-28 18:23:27 +0200

    Add all articles

--- src/components/Timeline/Section/Article/Image.js
+++ src/components/Timeline/Section/Article/Image.js
@@ -27,7 +27,77 @@ const Image = ({ alt, id }) => {
           }
         }
       }
-      veja1: file(relativePath: { eq: "articles/veja-1.jpg" }) {
+      intercept4: file(relativePath: { eq: "articles/intercept-4.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept5: file(relativePath: { eq: "articles/intercept-5.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept6: file(relativePath: { eq: "articles/intercept-6.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept7: file(relativePath: { eq: "articles/intercept-7.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept8: file(relativePath: { eq: "articles/intercept-8.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept9: file(relativePath: { eq: "articles/intercept-9.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept10: file(relativePath: { eq: "articles/intercept-10.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept11: file(relativePath: { eq: "articles/intercept-11.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept12: file(relativePath: { eq: "articles/intercept-12.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept13: file(relativePath: { eq: "articles/intercept-13.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      intercept1mes: file(relativePath: { eq: "articles/intercept-1mes.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
@@ -41,9 +111,147 @@ const Image = ({ alt, id }) => {
           }
         }
       }
+      folha2: file(relativePath: { eq: "articles/folha-2.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      folha3: file(relativePath: { eq: "articles/folha-3.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      folha4: file(relativePath: { eq: "articles/folha-3.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      folha5: file(relativePath: { eq: "articles/folha-5.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      folha6: file(relativePath: { eq: "articles/folha-6.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      folha7: file(relativePath: { eq: "articles/folha-7.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      folha8: file(relativePath: { eq: "articles/folha-8.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      folha9: file(relativePath: { eq: "articles/folha-9.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
       reinaldoazevedo1: file(
         relativePath: { eq: "articles/reinaldoazevedo-1.jpg" }
       ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      reinaldoazevedo2: file(
+        relativePath: { eq: "articles/reinaldoazevedo-2.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      reinaldoazevedo3: file(
+        relativePath: { eq: "articles/reinaldoazevedo-3.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      reinaldoazevedo4: file(
+        relativePath: { eq: "articles/reinaldoazevedo-4.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      reinaldoazevedo5: file(
+        relativePath: { eq: "articles/reinaldoazevedo-5.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      reinaldoazevedo6: file(
+        relativePath: { eq: "articles/reinaldoazevedo-6.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      reinaldoazevedo7: file(
+        relativePath: { eq: "articles/reinaldoazevedo-7.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      veja1: file(relativePath: { eq: "articles/veja-1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      veja2: file(relativePath: { eq: "articles/veja-2.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      veja3: file(relativePath: { eq: "articles/veja-3.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      veja4: file(relativePath: { eq: "articles/veja-4.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
