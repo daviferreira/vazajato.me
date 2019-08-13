8b3190c27e8c66052903f3fa20a0e9a1f98e3790
Davi Ferreira <contato@daviferreira.com>
2019-08-13 20:25:04 +0200

    Update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Timeline from '../components/Timeline';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Linha do tempo | #VazaJato" />
+    <SEO title="Linha do tempo da #VazaJato" />
     <Timeline />
   </Layout>
 );
