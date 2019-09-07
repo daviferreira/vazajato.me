a00c2bf6587628a56ba36bc211dc3db979738f93
Davi Ferreira <contato@daviferreira.com>
2019-09-07 08:01:02 +0200

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,6 @@ module.exports = {
         icon: `src/images/telegram-icon.png`
       }
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`
+    `gatsby-plugin-remove-serviceworker`
   ]
 };
