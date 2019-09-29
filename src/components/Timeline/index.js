9625e7686f3a1b9f05ad9a609541bd0e470107b2
Davi Ferreira <contato@daviferreira.com>
2019-09-29 10:20:34 +0200

    Add pagination
    Squashed commit of the following:
    
    commit 6ca7db3dc67b9975cad5cd4ee092c95e2df61ab5
    Author: Davi Ferreira <contato@daviferreira.com>
    Date:   Sun Sep 29 10:18:32 2019 +0200
    
        Use array item
    
    commit 0ec5fa7cdf485a71538e1bfa621707fcae0a82a8
    Author: Davi Ferreira <contato@daviferreira.com>
    Date:   Sun Sep 29 10:18:17 2019 +0200
    
        Update dependencies
    
    commit f15e68dcf24bf82646f4df1839a4ee717ee731f2
    Author: Davi Ferreira <contato@daviferreira.com>
    Date:   Sun Sep 29 10:14:34 2019 +0200
    
        Update articles
    
    commit fc267cfee5dddf6f965ee397e57842531db50ada
    Author: Davi Ferreira <contato@daviferreira.com>
    Date:   Sat Sep 21 18:57:50 2019 +0200
    
        Add padding
    
    commit dcfeb569e344584ab92689ccd8d4e1c54b17b9e7
    Author: Davi Ferreira <contato@daviferreira.com>
    Date:   Sat Sep 21 18:53:34 2019 +0200
    
        Clean up
    
    commit e2893a81fed75de97d57cb63bc131d1497d8130f
    Author: Davi Ferreira <contato@daviferreira.com>
    Date:   Sat Sep 21 18:51:19 2019 +0200
    
        Update articles
    
    commit 5d2f411bc5717edbdecceca886e8cd6203e100e3
    Author: Davi Ferreira <contato@daviferreira.com>
    Date:   Wed Sep 11 11:56:08 2019 +0200
    
        Wrap up pagination logic with sort
    
    commit 250d75fce24a832862f2ba71e9ab8a624d4d071e
    Author: Davi Ferreira <contato@daviferreira.com>
    Date:   Tue Sep 10 16:18:11 2019 +0200
    
        Paginate timeline

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,37 +1,113 @@
+import classnames from 'classnames';
 import groupBy from 'lodash.groupby';
 import map from 'lodash.map';
-import memoize from 'lodash.memoize';
 import React, { useState } from 'react';
+import { InView } from 'react-intersection-observer';
 
 import Section from './Section';
 import Sort from '../Sort';
 
-import articles from '../../data/articles.json';
+import data from '../../../static/data/2019-09.json';
 
 import styles from './styles.module.css';
 
-const groupArticles = memoize(order =>
-  groupBy(
-    order === 'asc' ? articles : articles.slice().reverse(),
-    ({ publishDate }) => publishDate.slice(0, 7)
-  )
-);
+const MONTHS = ['2019-09', '2019-08', '2019-07', '2019-06'];
 
 const Timeline = () => {
   const [order, setOrder] = useState('desc');
+  const [articles, setArticles] = useState(data);
+  const [months, setMonths] = useState(MONTHS.slice(1));
+  const [hasNext, setHasNext] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [cache, setCache] = useState({
+    [MONTHS[0]]: data
+  });
 
-  const groupedArticles = groupArticles(order);
+  const onSortChange = () => {
+    window.scrollTo(0, 0);
+
+    const nextOrder = order === 'asc' ? 'desc' : 'asc';
+
+    if (nextOrder === 'asc') {
+      setArticles([]);
+      setMonths(MONTHS.slice().reverse());
+    } else {
+      setArticles(data);
+      setMonths(MONTHS.slice(1));
+    }
+
+    setIsLoading(false);
+    setHasNext(true);
+    setOrder(nextOrder);
+  };
+
+  const groupedArticles = groupBy(
+    order === 'desc' ? articles.slice().reverse() : articles,
+    ({ publishDate }) => publishDate.slice(0, 7)
+  );
+
+  const onChange = inView => {
+    if (!isLoading && inView) {
+      return fetchArticles();
+    }
+  };
+
+  const fetchArticles = async () => {
+    const page = months[0];
+
+    setIsLoading(true);
+
+    if (cache[page]) {
+      const nextMonths = months.slice(1);
+
+      setMonths(nextMonths);
+
+      setArticles(
+        order === 'asc'
+          ? articles.concat(cache[page])
+          : cache[page].concat(articles)
+      );
+
+      setHasNext(!!nextMonths.length);
+
+      return setIsLoading(false);
+    }
+
+    let response;
+
+    try {
+      response = await fetch(`/data/${page}.json`);
+
+      const nextArticles = await response.json();
+
+      const nextMonths = months.slice(1);
+
+      setCache({ ...cache, [page]: nextArticles });
+
+      setIsLoading(false);
+      setArticles(
+        order === 'asc'
+          ? articles.concat(nextArticles)
+          : nextArticles.concat(articles)
+      );
+      setMonths(nextMonths);
+      setHasNext(!!nextMonths.length);
+    } catch (err) {
+      setIsLoading(false);
+    }
+  };
 
   let count = 0;
   let articlesCount = 0;
 
   return (
     <>
-      <Sort
-        onClick={() => setOrder(order === 'asc' ? 'desc' : 'asc')}
-        order={order}
-      />
-      <section className={styles.root}>
+      <Sort onClick={onSortChange} order={order} />
+      <section
+        className={classnames(styles.root, {
+          [styles.loaded]: !hasNext
+        })}
+      >
         {map(groupedArticles, (group, month) => {
           const articles = groupBy(group, 'publishDate');
 
@@ -44,6 +120,7 @@ const Timeline = () => {
               previousCount={articlesCount}
             />
           );
+
           count++;
 
           articlesCount += Object.keys(articles).length;
@@ -51,6 +128,20 @@ const Timeline = () => {
           return component;
         })}
       </section>
+      {hasNext && (
+        <InView
+          as="div"
+          className={styles.loaderContainer}
+          onChange={onChange}
+          rootMargin="800px 0px"
+        >
+          <div className={styles.loader}>
+            <div />
+            <div />
+            <div />
+          </div>
+        </InView>
+      )}
     </>
   );
 };
