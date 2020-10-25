a377c328a14af3301f7fea02d3989a4d0475f56e
Davi Ferreira <contato@daviferreira.com>
2020-10-25 13:03:41 +0100

    Remove current source

--- src/components/Timeline/Header/index.js
+++ src/components/Timeline/Header/index.js
@@ -1,7 +1,6 @@
 import classnames from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
-import map from 'lodash.map';
 
 import { Menu, MenuList, MenuButton, MenuItem } from '@reach/menu-button';
 import Sort from '../../Sort';
@@ -16,63 +15,67 @@ import styles from './styles.module.css';
 
 const sourceKeys = Object.keys(sources).sort();
 
-const Header = ({ onSourceChange, onSortChange, order, source }) => (
-  <div className={styles.root}>
-    <div className={styles.filter}>
-      <Menu>
-        {({ isExpanded }) => (
-          <>
-            <MenuButton
-              className={styles.button}
-              style={
-                source !== 'all'
-                  ? {
-                      boxShadow: `inset 0 -3px 0 ${sources[source].color}`,
-                    }
-                  : undefined
-              }
-            >
-              <span>
-                {source === 'all' ? (
-                  'Todos os veículos'
-                ) : (
-                  <Source source={source} />
-                )}
-              </span>
-              <span aria-hidden>
-                <Arrow
-                  className={classnames(styles.arrow, {
-                    [styles.expanded]: isExpanded,
-                  })}
-                />
-              </span>
-            </MenuButton>
-            <MenuList className={styles.menu}>
-              <MenuItem
-                className={styles.item}
-                key="all"
-                onSelect={() => onSourceChange('all')}
+const Header = ({ onSourceChange, onSortChange, order, source }) => {
+  const isAll = source === 'all';
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.filter}>
+        <Menu>
+          {({ isExpanded }) => (
+            <>
+              <MenuButton
+                className={styles.button}
+                style={
+                  !isAll
+                    ? {
+                        boxShadow: `inset 0 -3px 0 ${sources[source].color}`,
+                      }
+                    : undefined
+                }
               >
-                <div className={styles.avatar} />
-                <span>Todos os veículos</span>
-              </MenuItem>
-              {map(sourceKeys, (key) => (
-                <MenuItem
-                  className={styles.item}
-                  key={key}
-                  onSelect={() => onSourceChange(key)}
-                >
-                  <Source source={key} />
-                </MenuItem>
-              ))}
-            </MenuList>
-          </>
-        )}
-      </Menu>
+                <span>
+                  {isAll ? 'Todos os veículos' : <Source source={source} />}
+                </span>
+                <span aria-hidden>
+                  <Arrow
+                    className={classnames(styles.arrow, {
+                      [styles.expanded]: isExpanded,
+                    })}
+                  />
+                </span>
+              </MenuButton>
+              <MenuList className={styles.menu}>
+                {!isAll && (
+                  <MenuItem
+                    className={classnames(styles.item, styles.all)}
+                    key="all"
+                    onSelect={() => onSourceChange('all')}
+                  >
+                    <div className={styles.avatar} />
+                    <span>Todos os veículos</span>
+                  </MenuItem>
+                )}
+                {sourceKeys
+                  .filter((key) => key !== source)
+                  .map((key) => (
+                    <MenuItem
+                      className={styles.item}
+                      key={key}
+                      onSelect={() => onSourceChange(key)}
+                    >
+                      <Source source={key} />
+                    </MenuItem>
+                  ))}
+              </MenuList>
+            </>
+          )}
+        </Menu>
+      </div>
+      <Sort onClick={onSortChange} order={order} />
     </div>
-    <Sort onClick={onSortChange} order={order} />
-  </div>
-);
+  );
+};
 
 Header.propTypes = {
   onSourceChange: PropTypes.func,
