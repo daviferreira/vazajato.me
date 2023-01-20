83f85f6e3b85808168055f8b38f90371649f2243
Davi Ferreira <contato@daviferreira.com>
2023-01-20 16:26:38 +0100

    Remove width

--- src/fragments/images.js
+++ src/fragments/images.js
@@ -3,17 +3,13 @@ import { graphql } from 'gatsby';
 export const images = graphql`
   fragment articleImage on File {
     childImageSharp {
-      gatsbyImageData(width: 800, placeholder: BLURRED, layout: FULL_WIDTH)
+      gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
     }
   }
 
   fragment sourceImage on File {
     childImageSharp {
-      gatsbyImageData(
-        width: 72
-        formats: [AUTO, WEBP, AVIF]
-        layout: FULL_WIDTH
-      )
+      gatsbyImageData(formats: [AUTO, WEBP, AVIF], layout: FULL_WIDTH)
     }
   }
 `;
