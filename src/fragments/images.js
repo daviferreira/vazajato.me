2a2465805f0c75c922e19e281b6454a1f7b7fc4f
Davi Ferreira <contato@daviferreira.com>
2023-01-20 16:32:00 +0100

    Remove placeholder

--- src/fragments/images.js
+++ src/fragments/images.js
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby';
 export const images = graphql`
   fragment articleImage on File {
     childImageSharp {
-      gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
+      gatsbyImageData(formats: [AUTO, WEBP, AVIF], layout: FULL_WIDTH)
     }
   }
 
